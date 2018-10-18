c3469d1e349a4090a52ccb30a969a0fd151b0d9d
thisisjungeun <starfire199471@gmail.com>
2018-10-18 21:38:12 +0900

    Board

--- models/board.js
+++ models/board.js
@@ -1,23 +1,52 @@
 const mongoose = require('mongoose');
 
-// 작성자, 작성 시간, 글 내용,  T-Stop 번호, 추천수 
+// 작성자, 작성 시간, 글 내용,  T-Stop 번호, 추천수
 const Board = new mongoose.Schema({
+  username : {
+    type: String,
+    required: true,
+    unique: true
+  },
+  lat : {
+      type: Number,
+      required: true,
+  },
+  lng : {
+      type: Number,
+      require: true
+  },
+  title : {
+    type: String,
+    require: true,
+  },
+  description : {
+    type: String,
+    require: true,
+  },
+  image : {
+    type: String,
+    require: true
+  }
 }, {
     timestamps: true
 });
 
-// Board.statics.create = function(title, description, lat, lng, image) {
-//     return (new this({
-//       title: title,
-//       description: description,
-//       lat: lat,
-//       lng: lng,
-//       image: image
-//     })).save()
-// };
+Board.statics.create = function(username, lat, lng, title, description, image) {
+    return (new this({
+      username: username,
+      description: description,
+      lat: lat,
+      lng: lng,
+      image: image
+    })).save()
+};
 
 Board.statics.select = function(param) {
     return this.findOne(param);
+};
+
+Board.statics.selectAll = function(param) {
+  return this.find(param);
 }
 
 module.exports = mongoose.model('board', Board);
--- routes/board.js
+++ routes/board.js
@@ -1,19 +1,59 @@
 const returnCode = require('../returnCode');
 const router = require('express').Router();
-const travelStop = require('../models/board');
+const board = require('../models/board');
 const ValidCheck = require("../validCheck");
 const Util = require("../util");
 
-// no = T스탑 번호
+// id = T스탑 번호
 
 // T스탑에 글 쓰기
-router.post('/:no', (req, res) => {
+router.post('/:id/write', (req, res) => {
+  var username = req.body.username;
+  var lat = parseFloat(req.body.lat);
+  var lng = parseFloat(req.body.lng);
+  var content = req.body.content;
+  var image = req.body.image;
+
+  board.create(username, lat, lng, content, image)
+    .then(function() {
+        res.send(returnCode['board']['addSuccess']);
+    })
+    .catch(function(err) {
+        console.log(err);
+        res.status(500).send(returnCode['unknown']['error'])
+    })
+});
+
+//T스탑 글 수정
+router.post('/:id/edit', (req, res) => {
+
+});
+
+//T스탑 글 삭제
+router.post('/:id/delete', (req, res) => {
 
 });
 
 // T스탑 글 목록
-router.get('/:no', (req, res) => {
-
+router.get('/:id/list', (req, res) => {
+  board.selectAll({})
+    .then((result) => {
+      console.log(result);
+      var list = [];
+      for (i = 0; i < result.length; i++) {
+        list.push(
+          {
+            "name": result[i]['username'],
+            "content": result[i]['content'],
+            "location":{
+                "longitude": code[i]['lng'],
+                "latitude": code[i]['lat'],
+            }
+          }
+        );
+      }
+      res.send(list);
+    });
 });
 
 module.exports = router;
