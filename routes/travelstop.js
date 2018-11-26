abef1499e9a2591d61c74e4cc860834015593015
Jangtaejin <jtjisgod@gmail.com>
2018-11-26 20:01:58 +0900

    PWPWPW

--- routes/travelstop.js
+++ routes/travelstop.js
@@ -14,13 +14,14 @@ router.get('/:id', (req, res) => {
 
 
 
-router.get('/visited/:userID', (req, res) => {
+router.get('/visited/:userID', async (req, res) => {
     var userID = req.params.userID;
-    visitied.find({
-        user : userID
-    }).sort({date:-1}).then((r) => {
-        res.send(r);
+    var rows = await visitied.find({ user : userID }).sort({date:-1}).exec()
+    var outputs = [];
+    rows.forEach(row => {
+        outputs.push(await travelStop.findById(row['travelstop']).exec())
     })
+    res.send(outputs);
 });
 
 
