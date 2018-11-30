a88a95ae5db32942ba18ee61cfa3767dca8349be
Jangtaejin <jtjisgod@gmail.com>
2018-11-30 08:56:11 +0000

    Spareta

--- tstopsInserter.py
+++ tstopsInserter.py
@@ -7,7 +7,8 @@ if __name__ == "__main__" :
     f.close()
     
     for stop in stops['stops'] :
-        stop['image'] = "https://pds.joins.com/news/component/htmlphoto_mmdata/201408/08/htm_20140808162735l300l303.jpg"
-        res = requests.post("http://여행해.한국/travelstop/upload",
+        if not stops.get('image', '') :
+            stop['image'] = "https://pds.joins.com/news/component/htmlphoto_mmdata/201408/08/htm_20140808162735l300l303.jpg"
+        res = requests.post("http://localhost/travelstop/upload",
         data=stop)
         print(res.text)
